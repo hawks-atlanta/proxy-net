174abf099077bd34535957f2bfb84cadd7a53ab3
Santiago Gaona <53282017+SantiagoGaonaC@users.noreply.github.com>
2023-10-19 23:00:50 -0500

    feat: update ServiceReference WSDL (#35)

--- ServiceReference/Reference.cs
+++ ServiceReference/Reference.cs
@@ -1930,21 +1930,16 @@ namespace ServiceReference
             }
             throw new System.InvalidOperationException(string.Format("No se pudo encontrar un punto de conexión con el nombre \"{0}\".", endpointConfiguration));
         }
-
+        
         private static System.ServiceModel.EndpointAddress GetEndpointAddress(EndpointConfiguration endpointConfiguration)
         {
             if ((endpointConfiguration == EndpointConfiguration.ServiceImpPort))
             {
-                string serviceUrl = Environment.GetEnvironmentVariable("SERVICE_URL");
-                if (string.IsNullOrEmpty(serviceUrl))
-                {
-                    throw new ApplicationException("SERVICE_URL env not found!");
-                }
-                return new System.ServiceModel.EndpointAddress(serviceUrl);
+                return new System.ServiceModel.EndpointAddress("http://localhost:8080/gw/service");
             }
             throw new System.InvalidOperationException(string.Format("No se pudo encontrar un punto de conexión con el nombre \"{0}\".", endpointConfiguration));
         }
-
+        
         private static System.ServiceModel.Channels.Binding GetDefaultBinding()
         {
             return ServiceClient.GetBindingForEndpoint(EndpointConfiguration.ServiceImpPort);
